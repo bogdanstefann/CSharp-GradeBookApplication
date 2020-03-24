8ac7385c92d9cf9693cf35342cde9a786127ffda
Bogdan Stefan <bogdanstefann@gmail.com>
2020-03-24 17:31:29 +0200

    Added GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -12,5 +13,32 @@ namespace GradeBook.GradeBooks
             this.Type = Enums.GradeBookType.Ranked;
         }
 
+        public override char GetLetterGrade(double averageGrade)
+        {
+            var numberOfStudents = this.Students.Count;
+            if (numberOfStudents < 5)
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+
+            int indexPercentage = (int)Math.Ceiling(numberOfStudents * 0.2);
+            var sortedGrades = Students.OrderByDescending(student => student.AverageGrade).ToList();
+
+            if (sortedGrades[indexPercentage - 1].AverageGrade <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (sortedGrades[indexPercentage * 2 - 1].AverageGrade <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (sortedGrades[indexPercentage * 3 - 1].AverageGrade <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (sortedGrades[indexPercentage * 4 - 1].AverageGrade <= averageGrade)
+            {
+                return 'D';
+            }
+            return 'F';
+        }
     }
 }
